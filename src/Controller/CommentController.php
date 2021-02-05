b1093e3ea04f0eb67002274fa29c953c73f50de6
Marie Loeffler <mloeffler@macbook-mloeffler.int.basecom.eu>
2021-02-05 10:56:23 +0100

    logger added - before webpack

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,16 +14,18 @@ class CommentController extends AbstractController
 
 
     /**
-     * @Route("/comments/{id}/vote/{direction}")
+     * @Route("/comments/{id}/vote/{direction<up|down>}", methods="POST")
      */
-    public function commentVote($id, $direction)
+    public function commentVote($id, $direction, LoggerInterface $logger)
     {
         //todo database
 
         if($direction === 'up'){
             $currentVoteCount = rand(7, 100);
+            $logger->info('Voting up!');
         } else {
             $currentVoteCount = rand(0, 5);
+            $logger->info('Voting down!');
         }
         return new JsonResponse(['votes' => $currentVoteCount]);
     }
