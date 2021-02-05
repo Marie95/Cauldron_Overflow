16e0bcefe2c50f4a432b2a29c197a259c65d4a91
Marie Loeffler <mloeffler@macbook-mloeffler.int.basecom.eu>
2021-02-05 10:20:19 +0100

    twig and routing

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response("homepage");
+    }
+
+    /**
+     * @Route("/question/{id}")
+     */
+    public function show($id)
+    {
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+        return $this->render('question/show.html.twig', ['id' => $id, 'answers' => $answers]);
+    }
+
+}
