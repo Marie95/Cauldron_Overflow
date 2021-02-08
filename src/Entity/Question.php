5d49ab95b0991c65274930b8b59a156c58e2a3b0
Marie Loeffler <mloeffler@macbook-mloeffler.int.basecom.eu>
2021-02-08 13:11:51 +0100

    Question Entity-Class (first migration prepared)

---
+++ src/Entity/Question.php
@@ -0,0 +1,121 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="QuestionRepository::class")
+ */
+class Question
+{
+    /**
+     * @ORM\ID()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $question;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $askedAt;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param mixed $slug
+     */
+    public function setSlug($slug): void
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+
+    /**
+     * @param mixed $question
+     */
+    public function setQuestion($question): void
+    {
+        $this->question = $question;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAskedAt()
+    {
+        return $this->askedAt;
+    }
+
+    /**
+     * @param mixed $askedAt
+     */
+    public function setAskedAt($askedAt): void
+    {
+        $this->askedAt = $askedAt;
+    }
+
+
+}
